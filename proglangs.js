d24ddf716740f5fcd2b329d243bc0dc7858c944a
sigurdtk <sigurdtk15@gmail.com>
2013-10-10 13:25:46 +0200

    lab2 rettet
    Fikset noen feil

--- proglangs.js
+++ proglangs.js
@@ -1,7 +1,11 @@
-var element_unordered_list = document.getElementById("minListe");alert(element_unordered_list);element_unordered_list.style.color = "red";
+var element_unordered_list = document.getElementById("minListe");
 
 function printToOutputBox(selectobj){
  var e_proglangs = document.getElementById("proglangs");
  var e_outputbox = document.getElementById("outputbox");
  e_outputbox.innerHTML = "Du har selektert programmeringsspråket " + e_proglangs.options[selectobj.selectedIndex].text;
+ }
+  function konstruksjon ()
+{
+alert("Denne siden er under konstruksjon.");
 }
